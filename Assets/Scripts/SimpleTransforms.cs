07f5d05c055d7a2254c6825f72ea66a7bb78180c
Frans <fsantoso@liberty.edu>
2022-06-21 14:17:29 -0400

    added vars

--- Assets/Scripts/SimpleTransforms.cs
+++ Assets/Scripts/SimpleTransforms.cs
@@ -13,6 +13,6 @@ public class SimpleTransforms : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,1f,0);
+        transform.Rotate(1f,2f,0);
     }
 }
