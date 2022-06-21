d0cc498482681e3bc9880483786f2e09976b3724
Frans <fsantoso@liberty.edu>
2022-06-21 14:17:37 -0400

    added additional x rot

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    public int health;
+    public float reloadRate = 1.5f;
+    public string favoriteColor;
+    public bool isReady = false;
+    public Camera the_Camera;
+    public GameObject theSphere;
+
+
     // Start is called before the first frame update
     void Start()
     {
